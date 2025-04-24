55ce9346280b474d00d4cda5ecf4db43e55821e8
Roberto Duran <info@robertoduran.me>
2025-04-24 15:19:48 -0500

    🧬 add: assets and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Oxanium } from "next/font/google"
+import localFont from 'next/font/local'
 import '@rainbow-me/rainbowkit/styles.css'
 import Web3Provider from "@/components/Web3Provider"
 import { GlobalChat } from "@/app/components/GlobalChat"
@@ -11,6 +12,32 @@ const oxanium = Oxanium({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-oxanium",
+})
+
+const ggSans = localFont({
+  src: [
+    {
+      path: './fonts/gg_sans_Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/gg_sans_Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/gg_sans_Semibold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/gg_sans_Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-gg-sans',
 })
 
 export const metadata = {
@@ -25,8 +52,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning className={oxanium.variable}>
-      <body className="font-oxanium overflow-hidden h-screen">
+    <html lang="en" suppressHydrationWarning className={` ${ggSans.variable} ${oxanium.variable}`}>
+      <body className="font-sans overflow-hidden h-screen">
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Web3Provider>
             <ScrollArea className="h-screen" type="always">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import defaultTheme from "tailwindcss/defaultTheme"
 
 const config = {
   darkMode: ["class"],
@@ -20,6 +21,7 @@ const config = {
     },
     extend: {
       fontFamily: {
+        sans: ["var(--font-gg-sans)", ...defaultTheme.fontFamily.sans],
         oxanium: ["var(--font-oxanium)", "sans-serif"],
       },
       colors: {
